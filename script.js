74b390e89c3c77156f5688dc59dde8dea160ca0c
Jonah <donotreply@jonahkc.com>
2021-04-04 04:38:06 +0000

    made more links available and added a Not Working line

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 //https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ?wmode=transparent&amp;iv_load_policy=3&amp;autoplay=1&amp;html5=1&amp;showinfo=0&amp;rel=0&amp;modestbranding=1&amp;playsinline=0&amp;theme=light
 const base = "https://www.youtube-nocookie.com/embed/";
 const end = "?wmode=transparent&amp;iv_load_policy=3&amp;autoplay=0&amp;html5=1&amp;showinfo=0&amp;rel=0&amp;modestbranding=1&amp;playsinline=0&amp;theme=light";
-const strip_chars = /[^0-9a-zA-z]/
+const strip_timestamps = /\?t=\d+/;
 var submit = document.getElementById('submit');
 var url = document.getElementById('url');
 var video_holder;
@@ -14,8 +14,8 @@ submit.onclick = () => {
 		document.getElementById('instructions').remove();
 	}
 	link = url.value;
-	id = link.replace(strip_chars, "");
-	id = id.substr(link.replace(strip_chars, "").length - 11, 11);
+	id = link.replace(strip_timestamps, "");
+	id = id.substr(id.length - 11, 11);
 	video_holder = document.getElementById('video-holder').cloneNode(true);
 	video_holder.style.display = 'block';
 	video_holder.children[1].src = base + id + end;
